6dc8e5269e591ffef36bfff1875d71c15ed3bf4d
Julio Lopez <julioj.lopeza@gmail.com>
2022-08-04 16:47:32 +0200

    refactor our main view component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import Wrapper from './wrapper.js'
+import { createContext, useState } from 'react';
+import Wrapper from './Wrapper'
+import Screen from './Screen'
+import Button from './Button'
 import './App.css';
+import AnimatedTitle from './AnimatedTitle';
+import KeypadWrapper from './KeypadWrapper';
+import WhiteWrapper from './WhiteWrapper';
+import useCalculator from './useCalculator';
+
+export const MyContext = createContext();
 
 function App() {
+  const [result, calculate] = useCalculator()
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Calculator
-        </h1>
-        <Wrapper />
+        <Wrapper>
+          <Screen result={result}/>
+          <WhiteWrapper>
+            <KeypadWrapper>
+              {['1','2','3','4','5','6', '+','C', '='].map(value => <Button key={value} value={value} calculate={calculate}/>)}
+            </KeypadWrapper>
+            <WhiteWrapper>
+              <KeypadWrapper>
+
+                {['%', 'sq()', '&'].map(value => <Button key={value} value={value} calculate={calculate}/>)}
+              </KeypadWrapper>
+            </WhiteWrapper>
+          </WhiteWrapper>
+        </Wrapper>
+        <Wrapper>
+          <Screen result={result}/>
+          <WhiteWrapper>
+            <KeypadWrapper>
+              {['1','2','3','C', '='].map(value => <Button key={value} value={value} calculate={calculate}/>)}
+            </KeypadWrapper>
+          </WhiteWrapper>
+        </Wrapper>
+        <Wrapper>
+          <WhiteWrapper>
+            <KeypadWrapper>
+              {['1','2','3','C', '='].map(value => <Button key={value} value={value} calculate={calculate}/>)}
+            </KeypadWrapper>
+          </WhiteWrapper>
+          <AnimatedTitle text='1st grade calculator' />
+          <Screen result={result}/>
+        </Wrapper>
       </header>
     </div>
   );
